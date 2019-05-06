cdd595a0f8e1e56b81bd9fbfb36add3f3a5107d4
prdricardo <30416018+prdricardo@users.noreply.github.com>
2019-05-06 14:31:34 -0300

    Primeira Versão
    Versão com alguns bugs mas funcionando.

---
+++ js/mapa.js
@@ -0,0 +1,72 @@
+var map;
+var idInfoBoxAberto;
+var infoBox = [];
+var markers = [];
+
+function initialize() {	
+	var latlng = new google.maps.LatLng(-18.8800397, -47.05878999999999);
+	
+    var options = {
+        zoom: 5,
+		center: latlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    map = new google.maps.Map(document.getElementById("mapa"), options);
+}
+
+initialize();
+
+function abrirInfoBox(id, marker) {
+	if (typeof(idInfoBoxAberto) == 'number' && typeof(infoBox[idInfoBoxAberto]) == 'object') {
+		infoBox[idInfoBoxAberto].close();
+	}
+
+	infoBox[id].open(map, marker);
+	idInfoBoxAberto = id;
+}
+
+function carregarPontos() {
+	
+	$.getJSON('js/data.json', function(pontos) {
+		
+		var latlngbounds = new google.maps.LatLngBounds();
+		
+		$.each(pontos, function(index, ponto) {
+			
+			var marker = new google.maps.Marker({
+				position: new google.maps.LatLng(ponto.latitude, ponto.longitude),
+				//title: "Meu ponto personalizado! :-D",
+				//icon: 'img/marcador.png'
+			});
+			
+			var myOptions = {
+				content: "<p>" + ponto.municipio + "</p>",
+				pixelOffset: new google.maps.Size(-150, 0)
+        	};
+
+			/*infoBox[ponto.Id] = new InfoBox(myOptions);
+			infoBox[ponto.Id].marker = marker;
+			
+			infoBox[ponto.Id].listener = google.maps.event.addListener(marker, 'click', function (e) {
+				abrirInfoBox(ponto.Id, marker);
+			});*/
+			
+			markers.push(marker);
+			
+			latlngbounds.extend(marker.position);
+			
+		});
+		
+		
+		var markerCluster = new MarkerClusterer(map, markers,
+{imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+
+	
+		map.fitBounds(latlngbounds);
+		
+	});
+	
+}
+
+carregarPontos();
